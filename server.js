fc501b5bebd709bce80b24bbf28e01427867ac80
rahulapps29 <rahulapps29@gmail.com>
2024-12-08 14:30:57 +0530

    tenth

--- server.js
+++ server.js
@@ -35,17 +35,13 @@ app.use("/api/contacts", contactRoutes);
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected successfully"))
   .catch((err) => {
     console.error("MongoDB connection error:", err);
     process.exit(1); // Exit process if database connection fails
   });
 
-console.log("abc");
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
